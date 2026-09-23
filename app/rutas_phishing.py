a5f537de06f393bc64a80f16dd01309acc3f5cbd
arandaemon <alehernan.aran@hotmail.com>
2026-09-23 16:00:44 -0400

    Fix: OpSec blindado, mitigación CWE-312, Regex y escudos IP

--- app/rutas_phishing.py
+++ app/rutas_phishing.py
@@ -2,7 +2,7 @@ import logging
 import secrets
 from flask import Blueprint, render_template, session, abort, request, redirect, url_for
 from rutas_qrs import MAPEO_TRACKING
-from redis_db import detector_de_fases, registrar_victima, convertir_email_hash, limiter
+from redis_db import detector_de_fases, registrar_victima, convertir_email_hash, limiter, obtener_huella_ip
 import re
 
 # Para evitar que alguien se le ocurra usar un correo que no sea de la UAM para probar el sistema,
